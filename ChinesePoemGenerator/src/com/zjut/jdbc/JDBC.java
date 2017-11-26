b1f9e9e2bc7af5d71e638a23f7c520820cb767e8
admin <admin@10.12.42.153>
2017-11-26 14:30:51 +0800

    添加数据库连接配置

---
+++ ChinesePoemGenerator/src/com/zjut/jdbc/JDBC.java
@@ -0,0 +1,31 @@
+package com.zjut.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JDBC {
+	static final String url = "jdbc:mysql://localhost:3306/animal?useUnicode=true&characterEncoding=utf8";
+	static final String name = "com.mysql.jdbc.Driver";
+	static final String user = "root";
+	static final String password = "zjuta203";
+
+	public static Connection getConnection() {
+		Connection connection = null;
+
+		// 加载驱动程序
+		try {
+			Class.forName(name);
+			// 1.getConnection()方法，连接MySQL数据库！！
+			connection = DriverManager.getConnection(url, user, password);
+			if (!connection.isClosed())
+				System.out.println("Succeeded connecting to the Database!");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return connection;
+	}
+}
